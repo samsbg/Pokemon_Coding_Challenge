604fa1ce631c80055e4e8cc1c6fe225244c9fc41
Samantha <samanthasofiabg@gmail.com>
2023-09-27 22:43:26 -0600

    PokemonList functional component change

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,43 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { fetchData } from '../utils/api';
 import { sortData, filterData } from '../utils/helpers';
 import Filter from './Filter';
 import { List, Card } from 'antd';
 import { Link } from 'react-router-dom';
 
-class PokemonList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemon: [],
-      filter: '',
-      sort: 'name',
-    };
-  }
+function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [sort, setSort] = useState('name');
 
-  componentDidMount() {
-    fetchData('/pokemon')
-      .then(data => {
-        this.setState({ pokemon: data.results })})
-      .catch(error => console.error(error));
-  }
-
-  handleFilterChange = (filter) => {
-    this.setState({ filter });
-  }
-
-  handleSortChange = (sort) => {
-    this.setState({ sort });
-  }
-
- render() {
-  const { pokemon, filter, sort } = this.state;
+  fetchData('/pokemon')
+    .then(data => {setPokemon(data.results)})
+    .catch(error => console.error(error));
+  
   const filteredPokemon = filterData(pokemon, filter);
   const sortedPokemon = sortData(filteredPokemon, sort);
 
   return (
     <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
-      <Filter onFilterChange={this.handleFilterChange} onSortChange={this.handleSortChange} />
+      <Filter onFilterChange={setFilter} onSortChange={setSort} />
       <List
         style={{ background: '#f5f5f5', borderRadius: '10px', padding: '20px' }}
         grid={{ gutter: 16, column: 4 }}
@@ -59,7 +41,6 @@ class PokemonList extends Component {
       />
     </div>
   );
-}
 }
 
 export default PokemonList;
